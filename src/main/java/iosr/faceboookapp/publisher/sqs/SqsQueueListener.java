f2fc4223c875aef147a5879b31b627c71af88c02
leszek <leszekplaczkiewicz@gmail.com>
2017-12-07 14:56:29 +0100

    Parse sqs queue message.

--- src/main/java/iosr/faceboookapp/publisher/sqs/SqsQueueListener.java
+++ src/main/java/iosr/faceboookapp/publisher/sqs/SqsQueueListener.java
@@ -1,9 +1,12 @@
 package iosr.faceboookapp.publisher.sqs;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 /**
  * Created by Leszek Placzkiewicz on 28.11.17.
@@ -13,10 +16,17 @@ public class SqsQueueListener {
 
     private static final Logger LOG = LoggerFactory.getLogger(SqsQueueListener.class);
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @SqsListener("${publisher.queue.name}")
-    private void receiveMessage(String message) {
-        LOG.info("Received SQS message {}", message);
+    private void receiveMessage(String message) throws IOException {
+        SqsMessage sqsMessage = objectMapper.readValue(message, SqsMessage.class);
+        LOG.info("Received SQS message {}", sqsMessage);
+        handleSqsMessage(sqsMessage);
+    }
+
+    private void handleSqsMessage(SqsMessage message) {
+        //TODO - implement
     }
 
 }
