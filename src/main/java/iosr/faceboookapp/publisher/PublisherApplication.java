4ca4a02723e82641538d5569905c1a38466aa5ca
leszek <leszekplaczkiewicz@gmail.com>
2017-12-12 23:50:06 +0100

    Fix autoconfig.

--- src/main/java/iosr/faceboookapp/publisher/PublisherApplication.java
+++ src/main/java/iosr/faceboookapp/publisher/PublisherApplication.java
@@ -1,11 +1,28 @@
 package iosr.faceboookapp.publisher;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.ProxyCachingConfiguration;
+import org.springframework.cloud.aws.autoconfigure.cache.ElastiCacheAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.mail.MailSenderAutoConfiguration;
+import org.springframework.cloud.aws.core.env.stack.StackResourceRegistry;
+import org.springframework.cloud.aws.core.env.stack.config.StackResourceRegistryFactoryBean;
+import org.springframework.cloud.aws.messaging.config.annotation.SnsWebConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAutoConfiguration(exclude = {
+		ElastiCacheAutoConfiguration.class,
+		MailSenderAutoConfiguration.class,
+		StackResourceRegistryFactoryBean.class,
+		ContextStackAutoConfiguration.class,
+		StackResourceRegistry.class,
+		ProxyCachingConfiguration.class,
+		SnsWebConfiguration.class,
+})
 public class PublisherApplication {
 
 	public static void main(String[] args) {
